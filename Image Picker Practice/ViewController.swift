4e46e6eb566982cb354bc698c107f75da92b1f53
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-06-03 12:26:20 +0100

    Fixed bug for only moving view when bottom text field is editing

--- Image Picker Practice/ViewController.swift
+++ Image Picker Practice/ViewController.swift
@@ -89,35 +89,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //MARK: - Move view when keyboard appears on bottom text field
     
     
+    // - Move screen up
+    @objc func keyboardWillShow(_ notifictation: Notification) {
+        if bottomTextField.isEditing {
+        view.frame.origin.y = -getKeyboardHeight(notifictation)
+        }
+    }
+    // - Move screen down
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if view.frame.origin.y != 0 {
+        view.frame.origin.y += getKeyboardHeight(notification)
+        }
+    }
+    // - Calculate keyboard height
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
     // - Make view controller subscribe to keyboard notifications
+    
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
     // - Unsubscribe from keyboard notifications
     func unsubscribeFromKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    
-    // - Move screen up
-    @objc func keyboardWillShow(_ notifictation: Notification) {
-        
-        view.frame.origin.y -= getKeyboardHeight(notifictation)
-    }
-    
-    // - Move screen down
-    @objc func keyboardWillHide(_ notification: Notification) {
-        view.frame.origin.y += getKeyboardHeight(notification)
-    }
-    
-    // - Calculate keyboard height
-    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
-        return keyboardSize.cgRectValue.height
     }
 
     
